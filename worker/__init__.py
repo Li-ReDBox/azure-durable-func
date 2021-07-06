653d83c3037bc4dd1fd2b7880987b33551f57fb2
Jianfeng Li <jianfeng.li@pwc.com>
2021-07-06 20:53:20 +0930

    Make worker works harder

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,6 +1,9 @@
 # This function is not intended to be invoked directly. Instead it will be
 # triggered by an orchestrator function.
+import random
+import time
 
 
 def main(name: str) -> str:
+    time.sleep(random.randint(1, 10000)/1000)
     return f"Hello {name}!"
