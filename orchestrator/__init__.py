59b605f72a52b625b28754601e3a3e48f8ac4038
Jianfeng Li <jianfeng.li@pwc.com>
2021-07-06 16:11:24 +0930

    Create a simple HTTP triggered orchestrator with one worker

---
+++ orchestrator/__init__.py
@@ -0,0 +1,13 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an HTTP starter function.
+import azure.durable_functions as df
+
+
+def orchestrator_function(context: df.DurableOrchestrationContext):
+    result1 = yield context.call_activity('worker', "Tokyo")
+    result2 = yield context.call_activity('worker', "Seattle")
+    result3 = yield context.call_activity('worker', "London")
+    return [result1, result2, result3]
+
+
+main = df.Orchestrator.create(orchestrator_function)
