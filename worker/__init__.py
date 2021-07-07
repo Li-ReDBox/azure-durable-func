47a85ce114ede9eaa850f9221c8b2d9e7167bfa9
Jianfeng Li <jianfeng.li@pwc.com>
2021-07-07 10:54:00 +0930

    Tracking time spent in worker

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,9 +1,11 @@
 # This function is not intended to be invoked directly. Instead it will be
 # triggered by an orchestrator function.
+import datetime
 import random
 import time
 
 
 def main(name: str) -> str:
+    start = datetime.datetime.now().isoformat()
     time.sleep(random.randint(1, 10000)/1000)
-    return f"Hello {name}!"
+    return f"Hello {name}! Process duration: {start} - {datetime.datetime.now().isoformat()}"
